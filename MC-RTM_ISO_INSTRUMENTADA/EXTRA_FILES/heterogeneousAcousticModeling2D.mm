30dd4bb23d368600a19c5818a4daedb1e7ce28eb
Liliane <lilianeneves8@gmail.com>
2019-11-19 18:05:02 -0300

    Extra files folder

---
+++ MC-RTM_ISO_INSTRUMENTADA/EXTRA_FILES/heterogeneousAcousticModeling2D.mm
@@ -0,0 +1,9 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1537971992037" ID="ID_62463726" MODIFIED="1537973256786" TEXT="Full Waveform Inversion">
+<node CREATED="1537972046684" HGAP="99" ID="ID_1241971490" MODIFIED="1537972397958" POSITION="right" TEXT="Campo Direto" VSHIFT="-26"/>
+<node CREATED="1537972340744" HGAP="102" ID="ID_1128104837" MODIFIED="1537972401853" POSITION="right" TEXT="Res&#xed;duo" VSHIFT="-18"/>
+<node CREATED="1537972147891" HGAP="100" ID="ID_1586120206" MODIFIED="1537972438163" POSITION="right" TEXT="Campo Reverso"/>
+<node CREATED="1537972375402" HGAP="100" ID="ID_626797163" MODIFIED="1537972427782" POSITION="right" TEXT="Gradiente" VSHIFT="-53"/>
+</node>
+</map>
